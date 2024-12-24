66af1a86c06b10d6fed606a0b3cad6651c75e009
Roman <des8de@gmail.com>
2024-12-24 17:08:04 +0300

    mod

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import asyncio
-import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
-from dotenv import find_dotenv, load_dotenv
-
-load_dotenv(find_dotenv())
-
+from config_data.config import Config, load_config
 from middlewares.db import DataBaseSession
 from database.engine import create_db, drop_db, session_maker
 from handlers.user_private import user_private_router
@@ -15,7 +12,13 @@ from handlers.admin_private import admin_router
 from common.bot_cmds_list import private
 
 
-bot = Bot(token=os.getenv('BOT_TOKEN'), parse_mode=ParseMode.HTML)
+config: Config = load_config()
+
+
+bot = Bot(
+    token=config.token,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 
 dp = Dispatcher()
 
@@ -27,13 +30,8 @@ async def on_startup(bot):
     await create_db()
 
 
-async def on_shutdown(bot):
-    print('бот лег')
-
-
 async def main():
     dp.startup.register(on_startup)
-    dp.shutdown.register(on_shutdown)
 
     dp.update.middleware(DataBaseSession(session_pool=session_maker))
 
